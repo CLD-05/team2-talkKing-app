697f2c419375b6d51a5c317871ba010764506ede
just41ce <goldraz@naver.com>
2026-06-04 17:48:32 +0900

    fix: add test profile config for CI environment

--- talkking-chat/src/test/java/com/team2/talkking/TalkkingChatApplicationTests.java
+++ talkking-chat/src/test/java/com/team2/talkking/TalkkingChatApplicationTests.java
@@ -2,6 +2,7 @@ package com.team2.talkking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
 @ActiveProfiles("test")          // test 프로파일 사용
 class TalkkingChatApplicationTests {
